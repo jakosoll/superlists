3569fa0d3178d431fe1c80ae2c31da7ccb165b8a
Evgeniy Krivenko <jako.soll@gmail.com>
2020-10-31 10:09:29 +0300

    Первый модульный тест и преобразование url, фиктивное представление

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
+
+class TestHomePage(TestCase):
+    """Test home page"""
+
+    def test_url_resolves_to_home_page_view(self):
+        """root url resolves to home page view"""
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
 # Create your tests here.
