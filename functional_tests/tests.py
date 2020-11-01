76081ee237d7062f79c9d9918b70b99bb07e4475
Evgeniy Krivenko <jako.soll@gmail.com>
2020-11-01 19:46:26 +0300

    Добавил новый ФТ с новым функционалом

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -31,7 +31,7 @@ class NewUserTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         """Можно начать список и получить его позже"""
         # Эдит слышала про новое крутое приложение
         # со списком неотложных дел и решает оценить
@@ -71,11 +71,53 @@ class NewUserTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Купить павлиньи перья')
         self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
-        self.fail('Закончить тест!')
+        # Удовлетворенная она снова ложится спать.
 
         # Эдит интересно, запомнит ли сайт ее списко. Далее она видит,
         # что сайт сгенерировал для нее уникальный URL-адрес.
         # Об этом выводится небольшой текст с объяснениями.
 
         # Она посещает этот URL-адрес и ее список по-прежнему там.
-        # Удовлетворенная она снова ложится спать.
+
+    def test_multiple_users_can_start_list_at_different_urls(self):
+        """Тест: многочисленные пользователи могут начать списки по разным url"""
+        # Эдит начинает новый список
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Купить павлиньи перья')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+
+        # Она замечает, что ее список имеет уникальный URL-адрес
+        edit_list_url = self.browser.current_url
+        self.assertRegex(edit_list_url, '/lists/.+')
+
+        # Теперь приходит новый пользователь, Френсис.
+
+        ## Мы используем новый сеанс браузера, чтобы инфа Эдит не повлияла
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # Френсис посещает домашнюю страницу. Нет никаких призднаков Эдит
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать мушку', page_text)
+
+        # Френсис начинает новый список
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Купить молоко')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+
+        # Френсис получает уникальный адрес
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edit_list_url)
+
+        # Френсис еще раз убеждается, что нет следов списка Эдит
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Купить молоко', page_text)
+
+        # Удовлетворенные они оба ложатся спать
