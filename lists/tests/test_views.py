b04cfeafa601197e9486e595190f7b6a51b50edb
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-02 19:34:31 +0300

    Скорректировано новое представление списка с учетом валидации модели. ФТ не полностью проходят

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.utils.html import escape
 from lists.views import home_page
 from lists.models import Item, List
 
@@ -69,6 +70,18 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        """тест: ошибки валидации отсылаются назад в шаблон домашней страницы"""
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 class NewItemTest(TestCase):
     """Тест нового элемента списка"""
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from .models import Item, List
 
@@ -18,7 +19,14 @@ def view_list(request, pk):
 def new_list(request):
     """новый список"""
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
 
 
@@ -27,4 +35,6 @@ def add_item(request, pk):
     list_ = List.objects.get(id=pk)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
+
+# TODO: Удалеть захардкоженные URL-адреса в представлении
 
