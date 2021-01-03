f693cbb35e81c3ec5f9d48926f36916a54c94b1a
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-03 21:18:09 +0300

    Рефакториг предсталений. Добавил save() к форме. Тесты проходят. Начата реализация нового ФТ.

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.models import List, Item
 
 
 class ItemFormTest(TestCase):
@@ -9,7 +10,6 @@ class ItemFormTest(TestCase):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
-        # self.fail(form.as_p())
 
     def test_form_validation_for_blank_items(self):
         """тест валидации формы для пустных элментов"""
@@ -19,4 +19,13 @@ class ItemFormTest(TestCase):
             form.errors['text'],
             [EMPTY_ITEM_ERROR]
         )
+
+    def test_form_save_handles_saving_to_a_list(self):
+        """тест: метод save формы обрабатывает сохранение в список"""
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
 
--- lists/views.py
+++ lists/views.py
@@ -8,6 +8,17 @@ def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 
+def new_list(request):
+    """новый список"""
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
+
+
 def view_list(request, pk):
     """представление списка"""
     list_ = List.objects.get(id=pk)
@@ -15,19 +26,9 @@ def view_list(request, pk):
     if request.method == "POST":
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {
         'list': list_, 'form': form
     })
 
-
-def new_list(request):
-    """новый список"""
-    form = ItemForm(data=request.POST)
-    if form.is_valid():
-        list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
-        return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
