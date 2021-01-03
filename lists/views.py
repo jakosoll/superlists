a0e9bf1b7931ccf1f5524512da56830e2cc6b467
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-03 15:56:46 +0300

    Исправлен unit тест. Обеспечена волидация модели в представлении списка

--- lists/views.py
+++ lists/views.py
@@ -13,16 +13,23 @@ def home_page(request):
 def view_list(request, pk):
     """представление списка"""
     list_ = List.objects.get(id=pk)
+    error = None
     if request.method == "POST":
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect(f'/lists/{list_.id}/')
-    return render(request, 'list.html', {'list': list_})
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(f'/lists/{list_.id}/')
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
 
 def new_list(request):
     """новый список"""
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
