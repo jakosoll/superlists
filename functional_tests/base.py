f3ef6b47a8bf3147c7666335b3c9f693f861bd0b
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-11 19:51:31 +0300

    Декоратор ожидания. Рефактор ФТ. добавление фикстуры сессии

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,6 +7,19 @@ from selenium.common.exceptions import WebDriverException
 WAIT_TIME = 5
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (WebDriverException, AssertionError) as e:
+                if time.time() - start_time > WAIT_TIME:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     """
     Тестируем нового пользователя
@@ -20,42 +33,35 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         """Ожидать строки в таблице списка"""
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (WebDriverException, AssertionError) as e:
-                if time.time() - start_time > WAIT_TIME:
-                    raise e
-                time.sleep(0.5)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        return
 
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (WebDriverException, AssertionError) as e:
-                if time.time() - start_time > WAIT_TIME:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         """получить поле для ввода элемента"""
         return self.browser.find_element_by_id('id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
         """ожидать входа в систему"""
-        self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         """ожидать выхода"""
-        self.wait_for(lambda: self.browser.find_element_by_name('email'))
+        self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+
+
